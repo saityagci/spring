ede32702c5d8f5eee6de59dda5571af530c11740
saityagci <saityagci@gmail.com>
2022-09-18 17:01:12 -0400

    Abstraction

--- Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentNotificationProxy.java
@@ -1,6 +1,8 @@
 package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
+import org.springframework.stereotype.Component;
+
 
 public interface CommentNotificationProxy {
     void sendComment(Comment comment);
---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,22 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+//    @Autowired
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
