6de006caf3960a411552d1a9fcd56e2fb1791fbf
saityagci <saityagci@gmail.com>
2022-09-18 18:40:03 -0400

    @Qualifier

--- Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("EMAIL")
-public class EmailCommentNotificationProxy  implements CommentNotificationProxy{
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending Notification for comment: "+comment.getText());
+        System.out.println("Sending push notification for comment: "+comment.getText());
     }
 }
