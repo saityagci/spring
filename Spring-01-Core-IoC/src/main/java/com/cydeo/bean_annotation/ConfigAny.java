facf52a5223115f32f11c4f625729ccd06a0c859
saityagci <saityagci@gmail.com>
2022-09-18 02:06:58 -0400

    Bean Annotation

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigAny.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigAny.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ConfigApp {
+public class ConfigAny {
     @Bean
-    FullTimeMentor fullTimeMentor(){
-        return new FullTimeMentor();
+    String str(){
+        return "Developer";
     }
     @Bean
-    PartTimeMentor partTimeMentor(){
-        return new PartTimeMentor();
+    Integer number(){
+        return 44;
     }
 }
--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -6,6 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CydeoApp {
     public static void main(String[] args) {
 
-        ApplicationContext container=new AnnotationConfigApplicationContext();
+        ApplicationContext container=
+                new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+        FullTimeMentor ft=container.getBean(FullTimeMentor.class);
+        ft.createAccount();
+        PartTimeMentor pt=container.getBean(PartTimeMentor.class);
+        pt.createAccount();
+        String str=container.getBean(String.class);
+        System.out.println(str);
     }
 }
