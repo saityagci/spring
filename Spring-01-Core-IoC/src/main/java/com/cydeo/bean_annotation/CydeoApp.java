280425805c475306c69026c02eec63d16ad1f2ee
saityagci <saityagci@gmail.com>
2022-09-18 02:44:54 -0400

    Bean Annotation

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -10,7 +10,8 @@ public class CydeoApp {
                 new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
         FullTimeMentor ft=container.getBean(FullTimeMentor.class);
         ft.createAccount();
-        PartTimeMentor pt=container.getBean("p2",PartTimeMentor.class);
+      //  PartTimeMentor pt=container.getBean("p2",PartTimeMentor.class);
+        PartTimeMentor pt=container.getBean(PartTimeMentor.class);
         pt.createAccount();
         String str=container.getBean(String.class);
         System.out.println(str);
