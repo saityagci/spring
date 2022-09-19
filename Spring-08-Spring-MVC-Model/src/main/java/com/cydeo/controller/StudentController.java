179b90d5a68ee246aa784d48bb8171e2d7f7cd26
saityagci <saityagci@gmail.com>
2022-09-19 17:54:29 -0400

    Spring MVC- Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,9 @@ public class StudentController {
         numbers.add(45);
         numbers.add(49);
         model.addAttribute("numbers",numbers);
+
+        Student student=new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
         return "student/welcome";
     }
 
