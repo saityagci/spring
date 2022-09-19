7e2a02d2c73b42750942074d9956c62b28decda0
saityagci <saityagci@gmail.com>
2022-09-19 18:48:19 -0400

    Spring MVC- Model 2nd ex

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/AnotherController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/AnotherController.java
@@ -10,20 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mentor")
-public class MentorController {
-//    @RequestMapping("/welcome/mentor")
-//    public String mentorHomePage(Model model){
-//        Mentor mentor1=new Mentor("Mike","Smith", Gender.MALE,45);
-//        Mentor mentor2=new Mentor("Tom","Hanks", Gender.MALE,65);
-//        Mentor mentor3=new Mentor("Ammy","Bryan", Gender.FEMALE,25);
-//        model.addAttribute("mentor1",mentor1);
-//        model.addAttribute("mentor2",mentor2);
-//        model.addAttribute("mentor3",mentor3);
-//        return "mentor/welcome";
-//    }
-    @RequestMapping("list")   // localhost:8080/mentor/list
-    public String showTable(Model model) {
+@RequestMapping("/another")
+public class AnotherController {
+    @RequestMapping("/list")     // localhost:8080/another/list
+        public String showTable(Model model){
 
         List<Mentor> mentorList = new ArrayList<>();
         mentorList.add(new Mentor("Mike", "Smith", Gender.MALE, 45));
@@ -32,6 +22,7 @@ public class MentorController {
 
         model.addAttribute("mentors", mentorList);
         return "mentor/mentor-list";
-    }
 
+
+        }
 }
