64c41baa15ef9ea5f6eee0a6ab62c6f8beb72d2b
saityagci <saityagci@gmail.com>
2022-09-18 22:57:57 -0400

    Spring Boot Demo

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,21 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+    public static void main(String[] args) {
+        Comment comment=new Comment();
+        comment.setText("Spring Framework");
+        comment.setAuthor("Johnson");
+       ApplicationContext context= SpringApplication.run(CydeoApplication.class, args);
+        CommentService cs= context.getBean(CommentService.class);
+        cs.publishComment(comment);
+    }
+
+}
