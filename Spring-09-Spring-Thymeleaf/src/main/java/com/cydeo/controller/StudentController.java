ba801f4f2b5abb5b8d45162ae9e6308a8a11e71f
saityagci <saityagci@gmail.com>
2022-09-26 22:04:06 -0400

    Spring Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -15,5 +15,9 @@ public class StudentController {
         return "student/register";
     }
 
+    @RequestMapping("/welcome")
+    public String info(){
+        return "student/welcome";
+    }
 
 }
