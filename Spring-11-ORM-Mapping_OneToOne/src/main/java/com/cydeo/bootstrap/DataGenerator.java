cc1a9133ad88f0eb5f1e2181e2b00a560755b347
saityagci <saityagci@gmail.com>
2022-10-25 15:31:06 -0400

    ORM part3

--- Spring-11-ORM-Mapping_OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-11-ORM-Mapping_OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -3,15 +3,33 @@ package com.cydeo.bootstrap;
 import com.cydeo.entity.Department;
 import com.cydeo.entity.Employee;
 import com.cydeo.enums.Gender;
+import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
+    EmployeeRepository employeeRepository;
+    DepartmentRepository departmentRepository;
+
+    public DataGenerator(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
+        this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+
+
+        List<Employee> employeeList = new ArrayList<>();
+        List<Department> departmentList = new ArrayList<>();
 
         Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20),154864, Gender.F);
         Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26),56752, Gender.F);
@@ -24,6 +42,11 @@ public class DataGenerator implements CommandLineRunner {
         Department d3 = new Department("Clothing","Home");
         Department d4 = new Department("Phones & Tablets","Electronics");
         Department d5 = new Department("Computers","Electronics");
+        employeeList.addAll(Arrays.asList(e1,e2,e3,e4,e5));
+        departmentList.addAll(Arrays.asList(d1,d2,d3,d4,d5));
+        employeeRepository.saveAll(employeeList);
+        departmentRepository.saveAll(departmentList);
+
 
     }
 }
