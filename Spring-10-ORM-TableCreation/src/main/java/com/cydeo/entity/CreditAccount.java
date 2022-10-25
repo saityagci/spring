511adaa4411b5e1ed7c8e80ccf1dfbae1e01b7b1
saityagci <saityagci@gmail.com>
2022-10-25 02:26:07 -0400

    ORM part2

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/CreditAccount.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/CreditAccount.java
@@ -1,7 +1,8 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
 import java.math.BigDecimal;
-
+@Entity
 public class CreditAccount extends Account {
     BigDecimal creditLimit;
 }
--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
@@ -1,7 +1,8 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
 import java.math.BigDecimal;
-
+@Entity
 public class DebitAccount extends Account{
     private BigDecimal overDraftFee;
 }
