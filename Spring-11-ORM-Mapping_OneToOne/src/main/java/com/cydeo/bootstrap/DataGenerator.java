06d6b45b5b23dad456ace77916a0f0777e0d0b76
saityagci <saityagci@gmail.com>
2022-10-25 15:26:41 -0400

    ORM part3

--- Spring-11-ORM-Mapping_OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-11-ORM-Mapping_OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,4 +1,29 @@
 package com.cydeo.bootstrap;
 
-public class DataGenerator {
+import com.cydeo.entity.Department;
+import com.cydeo.entity.Employee;
+import com.cydeo.enums.Gender;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20),154864, Gender.F);
+        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26),56752, Gender.F);
+        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17),95313, Gender.F);
+        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014,03,02) ,119764,Gender.M);
+        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), 55307,Gender.M);
+
+        Department d1 = new Department("Sports","Outdoors");
+        Department d2 = new Department("Tools","Hardware");
+        Department d3 = new Department("Clothing","Home");
+        Department d4 = new Department("Phones & Tablets","Electronics");
+        Department d5 = new Department("Computers","Electronics");
+
+    }
 }
