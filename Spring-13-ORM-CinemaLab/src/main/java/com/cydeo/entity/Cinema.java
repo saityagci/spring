f76fee0a26865fe16dbfe07f5b194590fe194207
saityagci <saityagci@gmail.com>
2022-10-26 00:02:14 -0400

    CINEMA LAB

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -0,0 +1,15 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cinema extends BaseEntity{
+    private String name;
+    private String sponsoredName;
+    private Location location;
+}
