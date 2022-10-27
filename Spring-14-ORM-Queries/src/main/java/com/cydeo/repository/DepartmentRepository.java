c63bdacd37f6c73b9703f1ac3347354d4d0e401c
saityagci <saityagci@gmail.com>
2022-10-27 16:54:15 -0400

    Queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -16,5 +17,8 @@ public interface DepartmentRepository extends JpaRepository<Department,String> {
 
     //Display top 3 departments with division name includes 'Hea', without duplicates
     List<Department> findDistinctTop3ByDivisionContains(String pattern);
+
+    @Query("SELECT d FROM Department d WHERE d.division IN ?1")
+    List<Department> getDepartmentDivision(List<String> division);
 
 }
