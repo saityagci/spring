1356e3f81278581443fc9c3ce73ab12b7be3abca
saityagci <saityagci@gmail.com>
2022-10-27 20:53:00 -0400

    Repository

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,27 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.enums.UserRole;
+import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.CinemaRepository;
+import com.cydeo.repository.GenreRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+    private final AccountRepository accountRepository;
+    private final CinemaRepository cinemaRepository;
+    private final GenreRepository genreRepository;
+
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, GenreRepository genreRepository) {
+        this.accountRepository = accountRepository;
+        this.cinemaRepository = cinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(accountRepository.findByAgeBetween(11,23));
+
+    }
+}
