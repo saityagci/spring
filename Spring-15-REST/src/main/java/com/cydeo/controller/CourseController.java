3cffe181d72ffdc0af1df613674fd3462af49f82
saityagci <saityagci@gmail.com>
2022-11-30 18:00:17 -0500

    CourseController  PostMapping and PutMapping added

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -3,10 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +25,18 @@ public class CourseController {
     @GetMapping("{id}")
     public CourseDTO getCourseById(@PathVariable ("id") Long courseId){
         return courseService.getCourseById(courseId);
-
+    }
+    @GetMapping("/category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
+    }
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId,@RequestBody CourseDTO course){
+            courseService.updateCourse(courseId,course);
     }
 
 }
