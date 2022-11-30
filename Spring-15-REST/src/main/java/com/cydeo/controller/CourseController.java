37092311b3afa4881a37083ff1e104d6e941a3e0
saityagci <saityagci@gmail.com>
2022-11-30 17:46:11 -0500

    CourseController  getCourseById method created

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -4,13 +4,14 @@ import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RestController
+
+@RestController // @Controller+ ResponseBody
 @RequestMapping("/courses")
 public class CourseController {
     private  final CourseService courseService;
@@ -23,6 +24,11 @@ public class CourseController {
     public List<CourseDTO> getAllCourse(){
         List<CourseDTO> list=courseService.getCourses();
         return list;
+    }
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable ("id") Long courseId){
+        return courseService.getCourseById(courseId);
+
     }
 
 }
