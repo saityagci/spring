7385641c7ceaa55e9ae1f9af5065d5d1dff666e5
saityagci <saityagci@gmail.com>
2022-12-14 01:36:58 -0500

    Rest Consume

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -1,11 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.entity.Genre;
 import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,8 +16,10 @@ import javax.management.monitor.MonitorNotification;
 
 @RestController
 public class Consume_WebClient {
+    private WebClient webClient=WebClient.builder().baseUrl("http://localhost:8080").build();
     private final MovieCinemaRepository movieCinemaRepository;
     private final GenreRepository genreRepository;
+
 
     public Consume_WebClient(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
         this.movieCinemaRepository = movieCinemaRepository;
@@ -25,7 +30,39 @@ public class Consume_WebClient {
         return Flux.fromIterable(movieCinemaRepository.findAll());
     }
     @GetMapping("/mono-movie-cinema/{id}")
-    public Mono<MovieCinema> readById(@PathVariable("id") Long id){
-        return Mono.just(movieCinemaRepository.findById(id).get());
+    public ResponseEntity<Mono<MovieCinema>> readById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(Mono.just(movieCinemaRepository.findById(id).get()));
+    }
+    @PostMapping("/create-genre")
+    public Mono<Genre> createGenre(@RequestBody Genre genre){
+        Genre createGenre=genreRepository.save(genre);
+        return Mono.just(createGenre);
+      //  return Mono.just(genreRepository.save(genre));
+    }
+    @DeleteMapping("/delete/genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id){
+        genreRepository.deleteById(id);
+        return Mono.empty();
+    }
+
+
+    //-----------------------------WEBCLIENT-----------------------
+
+    @GetMapping("/flux")
+    public Flux<MovieCinema> readWithWebClient(){
+        return webClient
+                .get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(MovieCinema.class);
+    }
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema> readMonoWithWebClient(@PathVariable("id") Long id){
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}",id)
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
     }
 }
