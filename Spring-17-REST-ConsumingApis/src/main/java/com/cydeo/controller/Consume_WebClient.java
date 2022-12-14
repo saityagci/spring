4a5f3789de2e0d79cf28eb168027d2c87c3d7834
saityagci <saityagci@gmail.com>
2022-12-14 01:19:34 -0500

    Rest Consume

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -1,8 +1,15 @@
 package com.cydeo.controller;
 
+import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.management.monitor.MonitorNotification;
 
 @RestController
 public class Consume_WebClient {
@@ -12,5 +19,13 @@ public class Consume_WebClient {
     public Consume_WebClient(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
         this.movieCinemaRepository = movieCinemaRepository;
         this.genreRepository = genreRepository;
+    }
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readById(@PathVariable("id") Long id){
+        return Mono.just(movieCinemaRepository.findById(id).get());
     }
 }
