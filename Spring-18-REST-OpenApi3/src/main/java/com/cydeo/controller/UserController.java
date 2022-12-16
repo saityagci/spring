95ba75f75dbab8d468d723ffe8889aec492a2558
saityagci <saityagci@gmail.com>
2022-12-16 01:42:26 -0500

    Open api UserController and CinemaController tags added

--- Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/UserController.java
+++ Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.entity.User;
 import com.cydeo.repository.UserRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name="User", description = "User CRUD Operations")
 public class UserController {
     private final UserRepository userRepository;
 
