7c6e2082706ee447b25600cbeca8dcb67a69df79
saityagci <saityagci@gmail.com>
2023-01-12 23:38:52 -0500

    unit testing

---
+++ Spring-21-unitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void add(){
+//        int actual=Calculator.add(2,3);
+    }
+
+}
