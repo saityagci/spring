cadd3061acd60e83fa14e8dd03b3db432312ce8a
saityagci <saityagci@gmail.com>
2023-01-13 03:08:51 -0500

    mockito

--- Spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/UserServiceImplTest.java
+++ Spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/UserServiceImplTest.java
@@ -1,7 +1,25 @@
 package com.cydeo.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.cydeo.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
+    @Mock
+    UserRepository userRepository;
+    @InjectMocks
+    UserServiceImpl userService;
+    @Test
+    void deleteByUsername_test(){
+        userService.deleteByUserName("mikesmith@cydeo.com");
+//        Mockito.verify(userRepository).deleteByUserName("mikesmith@cydeo.com");
+    }
+
 
 }
